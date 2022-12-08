8a9602cad80d9c29c0b999e0e41eb14d6c7891ca
Nathan-Matthews <nate.matthews24@gmail.com>
2022-12-08 15:50:16 -0500

    Day5 Part1

---
+++ day5/script.py
@@ -0,0 +1,6 @@
+# Start by hardcoding a stack for each number, then track movements by pushing and pulling from the stacks.
+
+with open("input.txt") as f:
+    result = 0
+    for line in f:
+        print(line)
